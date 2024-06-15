efd26726b953da000337cf2d96dd539d67e011d6
shivam.jha1 <shivam.jha@pw.live>
2024-06-15 13:54:19 +0530

    electron setup

---
+++ public/preload.js
@@ -0,0 +1,13 @@
+// All of the Node.js APIs are available in the preload process.
+// It has the same sandbox as a Chrome extension.
+
+// JUST SHOWING VERIONS IN THE HELLO WORLD APP
+window.addEventListener("DOMContentLoaded", () => {
+  for (const versionType of ["chrome", "electron", "node"]) {
+    document.getElementById(`${versionType}-version`).innerText =
+      process.versions[versionType];
+  }
+
+  /* document.getElementById("serialport-version").innerText =
+    require("serialport/package").version; */
+});
