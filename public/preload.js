13af2d3e6ad87d3f727b0a4dca9989f1eae3cb37
shivam.jha1 <shivam.jha@pw.live>
2024-06-17 11:54:28 +0530

    some changes

--- public/preload.js
+++ public/preload.js
@@ -1,13 +1,5 @@
-// All of the Node.js APIs are available in the preload process.
-// It has the same sandbox as a Chrome extension.
+const { contextBridge, ipcRenderer } = require("electron");
 
-// JUST SHOWING VERIONS IN THE HELLO WORLD APP
-window.addEventListener("DOMContentLoaded", () => {
-  for (const versionType of ["chrome", "electron", "node"]) {
-    document.getElementById(`${versionType}-version`).innerText =
-      process.versions[versionType];
-  }
-
-  /* document.getElementById("serialport-version").innerText =
-    require("serialport/package").version; */
+contextBridge.exposeInMainWorld("electron", {
+  ipcRenderer: ipcRenderer,
 });
